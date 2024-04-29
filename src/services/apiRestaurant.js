957fa6683deba6e0c77baa18254a1cae845ded41
caleb mwema <calebmwema65@gmail.com>
2024-04-29 11:50:33 +0300

    Update apiRestaurant.js

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,4 +1,4 @@
-const API_URL = "https://jcb.onrender.com";
+const API_URL = "https://restaurant-erp-api.onrender.com";
 // const API_URL = "http://127.0.0.1:8000";
 const ACCESS_TOKEN = localStorage.getItem("accessToken");
 
